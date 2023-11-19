147ec95915720d0813a9265a5875011b82a22a83
Shirsendu Bairagi <shirsendu2001@gmail.com>
2023-11-19 20:29:43 +0530

    feat: tensorflow locally added

---
+++ Readme.md
@@ -0,0 +1,4 @@
+- cell phone
+- tv
+- person
+- bird
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,56 @@
+import '@tensorflow/tfjs-backend-webgpu';
+import '@tensorflow/tfjs-backend-webgl';
+import '@tensorflow/tfjs-backend-cpu';
+import * as tf from '@tensorflow/tfjs'
+
+import * as cocoSsd from '@tensorflow-models/coco-ssd';
+import * as poseDetection from '@tensorflow-models/pose-detection';
+
 import './index.css'
 
 const videoContainer = document.getElementById("video-container")
 const video = document.getElementById("video") as HTMLVideoElement
-const startBtn = document.getElementById("start-btn")
-const modelStatus = document.getElementById("model-status")
-const infoCamera = document.getElementById("info-camera")
-const infoObject = document.getElementById("info-object")
-let model: any = null;
+const actionBtn = document.getElementById("action-btn")
+const modelBtn = document.getElementById("model")
+const infoCamera = document.getElementById("camera-info")
+const objectsInfo = document.getElementById("objects-info")
+const pointsInfo = document.getElementById("points-info")
+const fpsElem = document.getElementById("fps")
+
+let modelObjectDetector: cocoSsd.ObjectDetection | null = null;
+let modelPostDetector: poseDetection.PoseDetector | null = null;
+const container = {
+	width: videoContainer?.clientWidth ?? 0,
+	height: videoContainer?.clientHeight ?? 0
+}
+const offset = {
+	x: 0,//-30,
+	y: 0//10
+}
 const camera = {
 	width: 0,
 	height: 0
 }
-const container = {
-	width: videoContainer?.clientWidth ?? 0,
-	height: videoContainer?.clientHeight ?? 0
+/* const objects: {
+	name: string;
+	x: number;
+	y: number;
+	w: number;
+	h: number;
+}[] = [] */
+const artifacts: { objectsHighlight: any[], objectsInfo: any[], pointsHighlight: any[], pointsInfo: any[] } =
+	{ objectsHighlight: [], objectsInfo: [], pointsHighlight: [], pointsInfo: [] }
+const fps: number[] = []
+
+function roundOff(num: number, significance = 2) {
+	return Math.round(num * (10 ** significance)) / (10 ** significance)
+}
+function runningAverage(nums: number[], newValue: number, bucketSize = 20) {
+	if (nums.length >= bucketSize)
+		nums.shift()
+
+	nums.push(newValue)
+	return nums.reduce((total, num) => total + num, 0) / nums.length
 }
 
 // Check webcam support
@@ -22,59 +59,121 @@ function getUserMediaSupport() {
 }
 
 async function modelInit() {
-	// @ts-ignore
-	model = await cocoSsd.load()
-	modelStatus!.innerText = "Model Loaded";
-	modelStatus?.classList.replace("bg-yellow-500", "bg-green-600")
+	await tf.ready();
+	await tf.setBackend('webgpu')
+
+	// modelObjectDetector = await cocoSsd.load();
+	modelPostDetector = await poseDetection.createDetector(poseDetection.SupportedModels.MoveNet, { modelType: poseDetection.movenet.modelType.SINGLEPOSE_THUNDER });
+
+	modelBtn?.classList.replace("bg-yellow-500", "bg-green-600")
 }
 
-const children: any[] = []
+function predictObjects(objectPredictions: cocoSsd.DetectedObject[]) {
+	for (let artifact of artifacts.objectsHighlight)
+		videoContainer?.removeChild(artifact);
+	for (let artifact of artifacts.objectsInfo)
+		objectsInfo?.removeChild(artifact);
 
-async function predict() {
-	for (let child of children)
-		videoContainer?.removeChild(child);
-	children.splice(0);
-	infoObject!.innerHTML = ""
+	artifacts.objectsHighlight.splice(0);
+	artifacts.objectsInfo.splice(0);
 
-	const predictions = await model.detect(video)
+	for (const prediction of objectPredictions) {
+		const { class: objectName, score: confidence, bbox: currBbox } = prediction
 
-	for (const prediction of predictions) {
-		const { bbox, class: objectName, score: confidence } = prediction
-
-		if (confidence < 0.66)
+		if (confidence < 0.5)
 			continue
 
-		const [x, y, width, height] = bbox
+		const [x, y, width, height] = currBbox
+		const [Tx, Ty, Twidth, Theight] = [(x / camera.width) * container.width + offset.x, (y / camera.height) * container.height - offset.y, (width / camera.width) * container.width, (height / camera.height) * container.height]
 
-		const highlight = document.createElement('div')
-		highlight.className = 'absolute bg-blue-500/20 rounded-md z-10 border'
-		highlight.style.left = (x / camera.width) * container.width + "px"
-		highlight.style.top = (y / camera.height) * container.height + "px"
-		highlight.style.width = (width / camera.width) * container.width + "px"
-		highlight.style.height = (height / camera.height) * container.height + "px"
+		const objectHighlight = document.createElement('div')
+		objectHighlight.className = 'absolute bg-blue-500/20 rounded-md z-10 border-2'
+		objectHighlight.style.left = Tx + "px"
+		objectHighlight.style.top = Ty + "px"
+		objectHighlight.style.width = Twidth + "px"
+		objectHighlight.style.height = Theight + "px"
 
-
-		infoObject!.innerHTML += `
-		<li>
-			<span>${objectName}</span>
-			<span>${Math.round(confidence * 100)} %</span>
-			<ul>
-				<li>X -> ${x}</li>
-				<li>Y -> ${y}</li>
-				<li>Width -> ${width}</li>
-				<li>Height -> ${height}</li>
+		const objectInfo = document.createElement('li')
+		objectInfo.className = 'flex flex-col gap-4 p-4 bg-slate-800 rounded-md transition-all'
+		objectInfo.innerHTML = `
+			<div id="${objectName}" class="flex gap-1 px-3 py-1 text-lg rounded-full capitalize bg-blue-500">
+				<img src="/icons/${objectName}.svg" class="w-6">	
+				<span>${objectName}</span>
+			</div>
+			<ul class="grid grid-rows-2 grid-cols-2">
+				<li>X &nbsp; ${roundOff(Tx)}</li>
+				<li>Y &nbsp; ${roundOff(Ty)}</li>
+				<li>W &nbsp; ${roundOff(Twidth)}</li>
+				<li>H &nbsp; ${roundOff(Theight)}</li>
 			</ul>
-			<ul>
-				<li>Transformed X -> ${(x / camera.width) * container.width}</li>
-				<li>Transformed Y -> ${(y / camera.height) * container.height}</li>
-				<li>Transformed Width -> ${(width / camera.width) * container.width}</li>
-				<li>Transformed Height -> ${(height / camera.height) * container.height}</li>
-			</ul>
-		<li>
 		`
-		videoContainer?.appendChild(highlight)
-		children.push(highlight)
+
+		videoContainer?.appendChild(objectHighlight)
+		objectsInfo?.appendChild(objectInfo)
+
+		artifacts.objectsHighlight.push(objectHighlight)
+		artifacts.objectsInfo.push(objectInfo)
 	}
+}
+
+function predictPoses(posePredictions: poseDetection.Pose[]) {
+	for (let artifact of artifacts.pointsHighlight)
+		videoContainer?.removeChild(artifact);
+	for (let artifact of artifacts.pointsInfo)
+		pointsInfo?.removeChild(artifact);
+
+	artifacts.pointsHighlight.splice(0);
+	artifacts.pointsInfo.splice(0);
+
+	for (const prediction of posePredictions) {
+		for (const keypoint of prediction.keypoints) {
+			const { name, score: confidence, x, y } = keypoint
+			const pointName = name?.split("_").join("-")
+
+			if (confidence && confidence < 0.5)
+				continue
+
+			const [Tx, Ty] = [(x / camera.width) * container.width + offset.x, (y / camera.height) * container.height - offset.y]
+
+			const pointHighlight = document.createElement('div')
+			pointHighlight.className = 'absolute bg-blue-500 rounded-full z-10 border-2 w-3 aspect-square'
+			pointHighlight.style.left = Tx + "px"
+			pointHighlight.style.top = Ty + "px"
+
+			const pointInfo = document.createElement('li')
+			pointInfo.className = 'flex flex-col gap-4 p-4 bg-slate-800 rounded-md transition-all'
+			pointInfo.innerHTML = `
+			<div class="flex gap-1 px-3 py-1 rounded-full bg-blue-500">
+				<img src="/icons/${pointName}.svg" class="w-6">	
+				<span class="text-lg capitalize">${pointName?.split("-").join(" ")}</span>
+			</div>
+			<ul class="grid grid-cols-2">
+				<li>X &nbsp; ${roundOff(Tx)}</li>
+				<li>Y &nbsp; ${roundOff(Ty)}</li>
+			</ul>
+		`
+
+			videoContainer?.appendChild(pointHighlight)
+			pointsInfo?.appendChild(pointInfo)
+
+			artifacts.pointsHighlight.push(pointHighlight)
+			artifacts.pointsInfo.push(pointInfo)
+		}
+
+	}
+}
+
+async function predict() {
+	const start = performance.now()
+
+	// const objectPredictions = await modelObjectDetector!.detect(video)
+	const posePredictions = await modelPostDetector!.estimatePoses(video);
+
+	// predictObjects(objectPredictions)
+	predictPoses(posePredictions)
+
+	const end = performance.now()
+	fpsElem!.innerText = `${roundOff(runningAverage(fps, 1000 / (end - start)), 0)} FPS`
 
 	window.requestAnimationFrame(predict)
 }
@@ -91,17 +190,21 @@ async function enableCam() {
 		camera.width = settings.width ?? 0
 		camera.height = settings.height ?? 0
 
+		infoCamera!.className = "flex flex-col gap-4 p-4 bg-slate-800 rounded-md"
 		infoCamera!.innerHTML = `
 		<ul>
-			<li>Width -> ${camera.width}</li>
-			<li>Height -> ${camera.height}</li>
-		</ul>`
-
+			<li>Camera Width -> ${camera.width}</li>
+			<li>Camera Height -> ${camera.height}</li>
+		</ul>
+		<ul>
+			<li>Container Width -> ${container.width}</li>
+			<li>Container Height -> ${container.height}</li>
+		</ul>
+		`
 		video.srcObject = stream
+		actionBtn!.children[0].src = "/icons/pause.svg"
+		actionBtn?.classList.replace("bg-blue-500", "bg-red-500")
 
-		startBtn!.innerHTML = "Stop"
-		startBtn?.classList.replace("bg-blue-500", "bg-red-500")
-		// 
 		video.addEventListener('loadeddata', predict)
 	} else {
 		alert("Your Device does't support Webcam")
@@ -110,4 +213,9 @@ async function enableCam() {
 
 modelInit()
 
-startBtn?.addEventListener('click', enableCam)
+actionBtn?.addEventListener('click', enableCam)
+
+window.addEventListener('resize', () => {
+	container.width = videoContainer?.clientWidth ?? 0
+	container.height = videoContainer?.clientHeight ?? 0
+})
